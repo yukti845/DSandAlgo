4e3bdb84a71f1e09fba4f6713e50e04531fb2479
Yukti Sachdeva <65419430+yukti845@users.noreply.github.com>
2021-04-14 23:14:44 +0530

    Update Doubly-Linked-Lists.py

--- Doubly-Linked-Lists.py
+++ Doubly-Linked-Lists.py
@@ -1,5 +1,9 @@
 # Doubly linked lists
-
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
 class DoublyLinkedList:
     def __init__(self):
