c2edd37021e1acb374c4d718288e2a5f4f77eea4
Yukti Sachdeva <65419430+yukti845@users.noreply.github.com>
2021-05-02 16:57:32 +0530

    Update Array rotation.py

--- Array rotation.py
+++ Array rotation.py
@@ -20,3 +20,13 @@ for i in range(T):
     for j in range(N):
         print(a[j])
     print('')
+
+# Hourglass max sum problem
+def hourglassSum(arr):
+    sum_hour = []
+    for i in range(4):
+        for j in range(4):
+            Sum = (arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + 
+                    arr[i+2][j+1] + arr[i+2][j+2])
+            sum_hour.append(Sum)
+    return max(sum_hour)
